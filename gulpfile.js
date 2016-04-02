f8f44aab17781fe61f7637cae8cbf1bd974b2957
Alexander Anpleenko <vaeum@yahoo.com>
2016-04-02 10:48:32 +0300

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var fs = require('fs');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 var jade = require('gulp-jade');
 var browserSync = require('browser-sync').create();
 
@@ -29,10 +30,9 @@ gulp.task('serve', ['sass', 'jade'], function() {
     browserSync.init({
         server: "./app"
     });
-
-    gulp.watch('./assets/sass/**/*.scss', ['sass']);
-    gulp.watch('./assets/jade/**/*.jade', ['jade']);
-    gulp.watch('./assets/data/data.json', ['jade']);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve'], function () {
+  watch(['./assets/jade/**/*.jade', './assets/data/data.json'], function () {gulp.start('jade')})
+  watch(['./assets/sass/**/*.scss'], function () {gulp.start('sass')})
+});
